bf21c2ac19a1df9af055e2519fadae776592eacf
nikhilm25 <134196172+nikhilm25@users.noreply.github.com>
2024-05-17 22:20:15 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import ReactDOM from "react-dom/client";
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
